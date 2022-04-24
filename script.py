fc58590ce83efe9942c5baba18ec2665ebb8ef15
Alesandro Silva <59420110+alleSilva@users.noreply.github.com>
2022-04-23 20:54:51 -0400

    Update script.py

--- script.py
+++ script.py
@@ -2,13 +2,15 @@ import re
 file1 = open('links.txt', 'w')
 array_links = []
 
-for index in range(1,11): 
-  with open("/home/alesandro/Downloads/message({}).txt".format(index)) as file:
-          for line in file:
-              re_url = re.findall('width":1280.*\.mp4', line)
-              url = re_url[0][47:].split(",")[0]
-              url= re.sub(r'.*:','https:', url)
-              url =re.sub(r'"+','', url)
-              array_links.append(url + "\n")
-
+for index in range(1,11):
+    with open("message({}).txt".format(index)) as file:
+        for line in file:
+            re_url = re.findall(r'width":1280.*\.mp4"', line)
+            if re_url == []:
+                array_links.append("### link not found - aula {} ###\n".format(index))
+            else:
+                url = re_url[0].split(",")[3]
+                url= re.sub(r'.*:','https:', url)
+                #url =re.sub(r'"+','', url)
+                array_links.append(url[:-1] + "\n")
 file1.writelines(array_links)
